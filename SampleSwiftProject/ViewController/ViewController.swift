cd217851834c71d50c5f0d0d0c07b36b5962155e
hiroshi egami <doegadoega@hotmail.com>
2025-06-03 14:05:27 +0900

    the frist commit

---
+++ SampleSwiftProject/ViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleSwiftProject
+//
+//  Created by Hiroshi Egami on 2025/05/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
