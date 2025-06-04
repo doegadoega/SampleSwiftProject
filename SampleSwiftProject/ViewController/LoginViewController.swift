a86ff3b8fcbc679d009b44159ce4af7370123bc0
hiroshi egami <doegadoega@hotmail.com>
2025-06-04 10:17:07 +0900

    the first commit

--- SampleSwiftProject/ViewController/LoginViewController.swift
+++ SampleSwiftProject/ViewController/LoginViewController.swift
@@ -8,22 +8,37 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    /// ログインタイトル
+    @IBOutlet private weak var loginTitleLabel: UILabel! {
+        didSet {
+            loginTitleLabel.text = "ログイン"
+        }
+    }
+    /// ログインIDテキストフィールド
+    @IBOutlet private weak var loginTextTextField: UITextField! {
+        didSet {
+            loginTextTextField.placeholder = "ログインIDを入力してください"
+        }
+    }
+    /// パスワードタイトル
+    @IBOutlet private weak var passwordTitleLabel: UILabel! {
+        didSet {
+            passwordTitleLabel.text = "パスワード"
+        }
+    }
+    /// パスワードテキスト
+    @IBOutlet private weak var passwordTextField: UITextField! {
+        didSet {
+            passwordTextField.placeholder = "パスワードを入力してください"
+            passwordTextField.textContentType = .password
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
