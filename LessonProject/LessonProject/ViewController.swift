46fc555a09361d49b9b4b9205931c15a821d2cda
hiroshi egami <doegadoega@hotmail.com>
2025-06-13 15:45:09 +0900

    LessonPojectを追加

---
+++ LessonProject/LessonProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LessonProject
+//
+//  Created by Hiroshi Egami on 2025/06/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
